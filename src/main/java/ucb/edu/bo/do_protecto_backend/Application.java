e310f18b8f66cc97ab3639c80cd937126b58ccf5
mauribanez <117604787+mauribanez@users.noreply.github.com>
2023-10-23 10:03:49 -0400

    first commit

---
+++ src/main/java/ucb/edu/bo/do_protecto_backend/Application.java
@@ -0,0 +1,13 @@
+package ucb.edu.bo.do_protecto_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
